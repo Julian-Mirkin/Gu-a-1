497651cc079d33a5c84ebe8464eec7a06ef6d772
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-03-17 10:41:18 -0300

    ej 1 terminado

--- Assets/scripts/DeclaracionDeVariables.cs
+++ Assets/scripts/DeclaracionDeVariables.cs
@@ -12,6 +12,16 @@ public class DeclaracionDeVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        EdadUsuario = 64;
+        precio = 1550.00f;
+        NombreUsuario = "Pepe";
+        vivo = true;
+
+        // escribo las variables en consola
+        Debug.Log(EdadUsuario);
+        Debug.Log(precio);
+        Debug.Log(NombreUsuario);
+        Debug.Log(vivo);
 
     }
 
