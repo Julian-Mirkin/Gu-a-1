eeba892f91af20be922680906409b67072ce8213
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-03-17 10:28:29 -0300

    variables creadas

--- Assets/scripts/DeclaracionDeVariables.cs
+++ Assets/scripts/DeclaracionDeVariables.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class DeclaracionDeVariables : MonoBehaviour
 {
+    int EdadUsuario;
+    float precio;
+    string NombreUsuario;
+    bool vivo;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
